e3ed6b6b9ccb84b4956f76b1ffe38f3ede15344f
maguerov <47131002+maguerov@users.noreply.github.com>
2020-11-06 13:30:20 -0600

    Commit Inicial
    Back end con pruebas en consola para el Create, Read.
    
    Update y delete estan creados y funcionales, pero dependen de el Search One para poder encontrar el elemento.
    
    El search one esta lanzado un error actualmente

---
+++ ProyectoComponentes/DataAccess/Entities/DynamoTable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Entities
+{
+    class DynamoTable
+    {
+        public string PrimaryKey { get; set; }
+        public string HashKey { get; set; }
+        public string RangeKey { get; set; }
+    }
+}
---
+++ ProyectoComponentes/Entities-POJO/Entities/Reservation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace Entities_POJO
+{
+    public class Reservation
+    {
+
+        [DynamoDBHashKey]
+        public string FullNameRentee { get; set; }
+        [DynamoDBRangeKey]
+        public int DateTime { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
